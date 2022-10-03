ddc392bca59449bf36286cb6d60746b5cede79c3
Jun_Water <tnwnsrla@naver.com>
2022-10-03 18:58:41 +0900

    Initial commit

---
+++ src/main/java/sweetswith/git_env_test/GitEnvTestApplication.java
@@ -0,0 +1,13 @@
+package sweetswith.git_env_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitEnvTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitEnvTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sweetswith/git_env_test/GitEnvTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sweetswith.git_env_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitEnvTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
